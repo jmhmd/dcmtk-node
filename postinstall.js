9fd13e41ac62fde4f781a3930d55cc28c1fca528
jmhmd <jhostetter@gmail.com>
2018-08-17 10:23:32 -0400

    Remove all binaries on non-target platforms

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const rimraf = require('rimraf');
 const { platform, BINARIES } = require('./src/check-platform')();
 
 const binariesToKeep = [
@@ -16,7 +17,8 @@ const binariesToKeep = [
 const installedAsModule = path.basename(path.resolve(__dirname, '..')) === 'node_modules';
 
 Object.keys(BINARIES).forEach((os) => {
-  if (os != platform) {
+  // on our platform, delete unnecessary binaries. on other platforms, delete all binaries
+  if (os == platform) {
     const files = fs.readdirSync(BINARIES[os]);
     files.forEach((file) => {
       const filename = path.basename(file, '.exe');
@@ -31,5 +33,12 @@ Object.keys(BINARIES).forEach((os) => {
         }
       }
     });
+  } else {
+    const dirPath = path.resolve(BINARIES[os], '..');
+    if (installedAsModule) {
+      rimraf.sync(dirPath);
+    } else {
+      console.log('Delete', dirPath);
+    }
   }
 });
